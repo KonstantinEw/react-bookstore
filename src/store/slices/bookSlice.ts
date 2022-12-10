b81465cc96ca67af30c625d1779ce50b97d26908
KonstantinEw <Ewwel@inbox.ru>
2022-12-10 19:15:23 +0300

    add(REACT-BOOKSTORE): add handler error

---
+++ src/store/slices/bookSlice.ts
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { restBooksAPI } from "../../services";
+import { IBook, IResponseSearch, ISearchResult } from "../../types";
+
+export const feachSearchBooks = createAsyncThunk<
+  IResponseSearch,
+  ISearchResult
+>("search/fetchSearchBooks", async (params) => {
+  return await restBooksAPI.searchBooks(params);
+});
+
+interface ISearchBook {
+  result: IResponseSearch;
+  isLoading: boolean;
+  error: null | string;
+}
+
+const initialState: ISearchBook = {
+  result: {} as IResponseSearch,
+  isLoading: false,
+  error: null,
+};
+
+const bookSlice = createSlice({
+  name: "newBooks",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(feachSearchBooks.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(feachSearchBooks.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.result.books = payload.books;
+      state.result.page = payload.page;
+    });
+    builder.addCase(feachSearchBooks.rejected, (state, { payload }: any) => {
+      state.isLoading = true;
+      state.result = payload;
+      state.error = payload;
+    });
+  },
+});
+
+export default bookSlice.reducer;
