0bd17f7e74d14cb1409ce0c7262d4deec19dcf28
KonstantinEw <Ewwel@inbox.ru>
2022-12-21 15:01:13 +0300

    fix(services): fix get book api

--- src/services/ibooksApi.ts
+++ src/services/ibooksApi.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import {
+  IBookIsbn13,
   IResponseBook,
   IResponseNewBooks,
   IResponseSearch,
   ISearchResult,
-} from "../types";
+} from "types";
 
 class booksAPI {
   private readonly BASE_URL = "https://api.itbook.store/1.0/";
@@ -25,14 +26,12 @@ class booksAPI {
   //починить опции
   public async searchBooks({ searchValue, page }: ISearchResult) {
     const { data } = await this.API.get<IResponseSearch>(
-      `${this.ENDPOINTS.search}/${searchValue}/${page}`
+      `${this.ENDPOINTS.search}/${searchValue}/${page}`,
     );
     return data;
   }
-  public async getBook(isbn13: string) {
-    const { data } = await this.API.get<IResponseBook>(
-      `${this.ENDPOINTS.books}/${isbn13}}`
-    );
+  public async getBook({ isbn13 }: IBookIsbn13) {
+    const { data } = await this.API.get<IResponseBook>(`${this.ENDPOINTS.books}/${isbn13}`);
     return data;
   }
 }
