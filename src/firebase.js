4713834a8c92c1ba394bb0e052a9d6d47d1ca752
KonstantinEw <Ewwel@inbox.ru>
2022-12-22 15:04:21 +0300

    feat(root): add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKY-7kOyegfVKUznJIKDfprul92kkuKi8",
+  authDomain: "book-store-ed78c.firebaseapp.com",
+  projectId: "book-store-ed78c",
+  storageBucket: "book-store-ed78c.appspot.com",
+  messagingSenderId: "508039719750",
+  appId: "1:508039719750:web:8905f74ecf849a8f00d347",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
