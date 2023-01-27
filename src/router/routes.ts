3a4f00c1a0387ac353a96fbaca1791e7014e32e7
Konstantin Ostorovich <Ewwel@inbox.ru>
2023-01-27 16:58:46 +0300

    fix(routes): fix routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,10 @@
 export enum ROUTE {
-  HOME = "react-bookstore/",
+  HOME = "/",
   SEARCH = "search/:searchValue/:page",
   FAVORITES = "favorites",
   PROFILE = "profile",
   SIGNUP = "auth",
   SHOP_BAG = "order",
-  DETAILS = "/books/:isbn13",
-  RESETPASS = "/reset",
+  DETAILS = "books/:isbn13",
+  RESETPASS = "reset",
 }
